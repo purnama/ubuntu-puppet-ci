d58cb19a7ef550632b467c20fb2870c6b3e74c77
Arthur Purnama <arthur@purnama.de>
2014-01-07 01:20:08 +0100

    Remove git and tomcat7 from install package list

--- puppet/modules/bootstrap/manifests/init.pp
+++ puppet/modules/bootstrap/manifests/init.pp
@@ -4,15 +4,9 @@ class bootstrap {
         ensure => 'present'
     }
 
-    # make sure the packages are up to date before beginning
-    exec { 'apt-get update':
-        command => '/usr/bin/apt-get update',
-    }
-
     # List all package need to be installed
-    $packages = ["curl", "vim", "subversion", "git", "make", "maven", "alien", 
+    $packages = ["curl", "vim", "subversion", "make", "maven", "alien", 
         "libaio1", "unixodbc", "unzip", "bc", "zip", "openjdk-7-jdk", "openjdk-7-source", 
-        "openjdk-7-demo", "openjdk-7-doc", "openjdk-7-jre-headless", "openjdk-7-jre-lib", 
-        "tomcat7"]
+        "openjdk-7-demo", "openjdk-7-doc", "openjdk-7-jre-headless", "openjdk-7-jre-lib"]
 
 }
